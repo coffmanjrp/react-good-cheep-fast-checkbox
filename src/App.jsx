407f7b0b9b7de62b1610d82ba53614dc01b1d8d8
coffmanjrp <coffmanjrp@gmail.com>
2021-08-22 20:52:22 -0700

    Implement checkbox event

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,62 @@
+import { useRef } from 'react';
 import './App.scss';
 
 function App() {
+  const good = useRef();
+  const cheep = useRef();
+  const fast = useRef();
+
+  const handleChange = (e) => {
+    if (good.current.checked && cheep.current.checked && fast.current.checked) {
+      if (good.current === e.target) {
+        fast.current.checked = false;
+      }
+      if (cheep.current === e.target) {
+        good.current.checked = false;
+      }
+      if (fast.current === e.target) {
+        cheep.current.checked = false;
+      }
+    }
+  };
+
   return (
     <div className="App">
       <h2>How do you want your project to be?</h2>
       <div className="toggle-container">
-        <input type="checkbox" id="good" className="toggle" />
+        <input
+          ref={good}
+          type="checkbox"
+          id="good"
+          className="toggle"
+          onChange={(e) => handleChange(e)}
+        />
         <label htmlFor="good" className="label">
           <div className="ball" />
         </label>
         <span>Good</span>
       </div>
       <div className="toggle-container">
-        <input type="checkbox" id="cheep" className="toggle" />
+        <input
+          ref={cheep}
+          type="checkbox"
+          id="cheep"
+          className="toggle"
+          onChange={(e) => handleChange(e)}
+        />
         <label htmlFor="cheep" className="label">
           <div className="ball" />
         </label>
         <span>Cheep</span>
       </div>
       <div className="toggle-container">
-        <input type="checkbox" id="fast" className="toggle" />
+        <input
+          ref={fast}
+          type="checkbox"
+          id="fast"
+          className="toggle"
+          onChange={(e) => handleChange(e)}
+        />
         <label htmlFor="fast" className="label">
           <div className="ball" />
         </label>
