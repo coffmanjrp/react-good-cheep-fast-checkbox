a4c7206ab04d7b0db9d991e515e73601a3ee174b
coffmanjrp <coffmanjrp@gmail.com>
2021-08-22 20:44:54 -0700

    HTML and styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,28 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h2>How do you want your project to be?</h2>
+      <div className="toggle-container">
+        <input type="checkbox" id="good" className="toggle" />
+        <label htmlFor="good" className="label">
+          <div className="ball" />
+        </label>
+        <span>Good</span>
+      </div>
+      <div className="toggle-container">
+        <input type="checkbox" id="cheep" className="toggle" />
+        <label htmlFor="cheep" className="label">
+          <div className="ball" />
+        </label>
+        <span>Cheep</span>
+      </div>
+      <div className="toggle-container">
+        <input type="checkbox" id="fast" className="toggle" />
+        <label htmlFor="fast" className="label">
+          <div className="ball" />
+        </label>
+        <span>Fast</span>
+      </div>
     </div>
   );
 }
